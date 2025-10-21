4d2682ab33293f742ee14de11865660b96058ed3
Rizky Ramadan <ramadanrizky593@gmail.com>
2025-10-21 20:46:14 +0700

    Update tugas pertemuan 6 Laravel

--- routes/api.php
+++ routes/api.php
@@ -5,14 +5,34 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\AuthController;
 
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::apiResource('/books', BookController::class);
+Route::apiResource('/genres', GenreController::class);
+Route::apiResource('/authors', AuthorController::class);
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
 
+Route::apiResource('/books', BookController::class)->only(['index', 'show']);
+Route::apiResource('/genres', GenreController::class)->only(['index', 'show']);
+Route::apiResource('/authors', AuthorController::class)->only(['index', 'show']);
+
+Route::middleware(['auth:api'])->group(function () {
+
+    Route::middleware(['role:admin'])->group(function () {
+        Route::apiResource('/books', BookController::class)->only(['store', 'update', 'destroy']);
+        Route::apiResource('/genres', GenreController::class)->only(['store', 'update', 'destroy']);
+        Route::apiResource('/authors', AuthorController::class)->only(['store', 'update', 'destroy']);
+    });
+
+});
 
 // Route::get('/books', [BookController::class, 'index']);
 // Route::post('/books', [BookController::class, 'store']);
@@ -20,7 +40,6 @@ Route::get('/user', function (Request $request) {
 // Route::delete('/books/{id}', [BookController::class, 'destroy']);
 // Route::post('/books/{id}', [BookController::class, 'update']);
 
-Route::apiResource('/books', BookController::class);
 
 // Route::get('/genres', [GenreController::class, 'index']);
 // Route::post('/genres', [GenreController::class, 'store']);
@@ -28,12 +47,9 @@ Route::apiResource('/books', BookController::class);
 // Route::delete('/genres/{id}', [GenreController::class, 'destroy']);
 // Route::post('/genres/{id}', [GenreController::class, 'update']);
 
-Route::apiResource('/genres', GenreController::class);
 
 // Route::get('/authors', [AuthorController::class, 'index']);
 // Route::post('/authors', [AuthorController::class, 'store']);
 // Route::get('/authors/{id}', [AuthorController::class, 'show']);
 // Route::delete('/authors/{id}', [AuthorController::class, 'destroy']);
 // Route::post('/authors/{id}', [AuthorController::class, 'update']);
-
-Route::apiResource('/authors', AuthorController::class);
